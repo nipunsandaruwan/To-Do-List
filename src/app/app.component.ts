26ebe0947a8ae0ba0e0b3ed371be8616f22c3479
nipunsandaruwan <nipunsandaruwan47@gmail.com>
2022-06-16 13:30:25 +0530

    version 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'to_do_list';
+  list:any[]=[];
+
+  addTask (item:string){
+    console.warn(item);
+    this.list.push({id:this.list.length,name:item})
+  }
+
+  removeTask(id:number){
+    console.log(id);
+    this.list=this.list.filter(item=>item.id |= id);
+    
+  }
+
+  name:any[]=[];
+  getName(){
+    this.name.push('hi','aedfaf');
+    
+    console.log(this.name);
+  }
+  
 }
